853461e9d80ca197280ffa3ea2e051b127d42c2f
Homayoon Farrahi <homayoonfarrahi@yahoo.com>
2014-12-23 17:22:44 +0330

    tested target ball path creation randomly based on kicks of the music, however it results in some unpredictable not necessarily good target movement. switching to random but predetermined path creation at the beginning of each play session.

--- app.js
+++ app.js
@@ -1,93 +1,167 @@
-var context = null
-var songBuffer = null
-var floatData = false
-window.addEventListener('load', init, false)
+var context = null;
+var floatData = false;
+window.addEventListener('load', init, false);
 
-var loadSong = function(url) {
-	var request = new XMLHttpRequest()
-	request.open('GET', url, true)
-	request.responseType = 'arraybuffer'
-	
-	// Decode asynchronously
-	request.onload = function() {
-		context.decodeAudioData(request.response, function (buffer) {
-			songBuffer = buffer
-			playSong(songBuffer)
-		}, function(error) {
-			console.log(error)
-		})
-	}
-	
-	request.send()
-}
+var loadSong = function(fileName) {
+	var audio = new Audio();
+	audio.src = fileName;
+	audio.controls = true;
+	audio.autoplay = true;
+	document.body.appendChild(audio);
+	playSong(audio);
+};
 
-var playSong = function(buffer) {
-	var source = context.createBufferSource()   // creates a sound source
-	source.buffer = buffer                      // tell the source which sound to play
+var playSong = function(audio) {
+	var prevFrameCount = 0;
+    var currFrameCount = 0;
+    var deltaFrameCount = 0;
+    var velocityTheta = 0;
 
-	var analyser = context.createAnalyser()
-	analyser.fftSize = 2048
-    analyser.smoothingTimeConstant = 0
-    analyser.maxDecibels = 0
-    //analyser.minDecibels = -130
-    if (!floatData)
-        var frequencyData = new Uint8Array(analyser.frequencyBinCount)
-    else
-        var frequencyData = new Float32Array(analyser.frequencyBinCount)
+    //*
+    var dancer = new Dancer();
+    dancer.load(audio);
+    //dancer.setVolume(0);
+    dancer.play();
 
-	source.connect(analyser)                    // connect the source to the analyser
-	analyser.connect(context.destination)       // connect the analyser to audio context destination (speakers)
+	/*
+    dancer.onceAt(5, function() {
+		var audio2 = new Audio();
+		audio2.src = './songs/getlucky.mp3';
+		audio2.controls = true;
+		audio2.autoplay = true;
+		document.body.appendChild(audio2);
 
+	    var dancer2 = new Dancer();
+	    dancer2.load(audio2);
+	    console.log(dancer2.isLoaded());
+	    dancer2.play();
+	    console.log(dancer2.isPlaying());
+    });
+    //*/
+
+
+    var kickColor = 'rgba(0, 10, 255, 1)';
+    kick = dancer.createKick({
+    	frequency: [0, 10],
+    	threshold: 0.4,
+    	decay: 0.02,
+    	onKick: function(mag) {
+    		//console.log('kick: ' + mag);
+    		kickColor = 'rgba(100, 10, 200, 0.5)';
+
+            deltaFrameCount = currFrameCount - prevFrameCount;
+            prevFrameCount = currFrameCount;
+    	},
+    	offKick: function(mag) {
+    		kickColor = 'rgba(200, 10, 100, 0.5)';
+    	}
+    }).on();
 
 
 	// setup graphic objects// Make an instance of two and place it on the page.
 	var elem = document.getElementById('draw-shapes').children[0];
-	var params = { fullscreen: true, autostart: true };
+	var params = { type: Two.Types['webgl'], fullscreen: true, autostart: true };
 	var two = new Two(params).appendTo(elem);
-    var barGroup = two.makeGroup()
-
-	for (var i=0 ; i<analyser.frequencyBinCount ; ++i) {
-        var barWidth = (window.innerWidth - 20) / analyser.frequencyBinCount
-        var bar = two.makeLine(10 + i*barWidth, 650, 10 + i*barWidth, 648)
-        bar.fill = 'rgba(255, 127, 0, 0.75)'
-        bar.linewidth = barWidth * 0.5
-        bar.stroke = 'rgba(255, 10, 0, 1)'
-        bar.addTo(barGroup)
+    var barGroup = two.makeGroup();
+/*
+    var tempSpectrum = dancer.getSpectrum();
+	for (var i=0 ; i<tempSpectrum.length ; ++i) {
+        var barWidth = (window.innerWidth - 20) / tempSpectrum.length;
+        var bar = two.makeLine(10 + i*barWidth, 650, 10 + i*barWidth, 648);
+        bar.fill = 'rgba(255, 127, 0, 0.75)';
+        bar.linewidth = barWidth * 0.5;
+        bar.stroke = 'rgba(255, 10, 0, 1)';
+        bar.addTo(barGroup);
     }
+*/
+
+    var targetBall = two.makeCircle(650, 300, 60);
+    targetBall.fill = 'rgba(200, 10, 100, 0.5)';
+    targetBall.noStroke();
+
+    var curve = two.makeCurve(650, 300, 850, 300, 850, 525, 725, 550, true);
+    curve.noFill().linewidth = 15;
+    curve.cap = curve.join = 'round';
+    curve.stroke = 'lightblue';
+    console.log(curve);
+
+    // var curve2 = two.makeCurve(850, 450, 850, 525, 825, 550, true);
+    // curve2.noFill().linewidth = 15;
+    // curve2.cap = curve2.join = 'round';
+    // curve2.stroke = 'pink';
+    // console.log(curve2);
+
+
+    curve.getPointAt(0.5, targetBall.translation);
+    targetBall.translation.addSelf(curve.translation);
+
+    var targetBallVelocity = new Two.Vector(1, 0);
 
 	// start the animation loop
-	two.bind('update', function(frameCount) {
-        if (!floatData)
-            analyser.getByteFrequencyData(frequencyData)
-        else
-            analyser.getFloatFrequencyData(frequencyData)
+	two.bind('update', function(frameCount, timeDelta) {
+		currFrameCount = frameCount;
 
-        var index = 0
+        //console.log(frameCount);
+        //if (timeDelta > 17)
+            //console.log(timeDelta);
+		//var frequencyData = dancer.getSpectrum();
+
+		/*
+        var index = 0;
         for(i in barGroup.children) {
-            var bar = barGroup.children[i]
-            var vertex = bar.vertices[1]
-            if (!floatData)
-                vertex.set(vertex.x, -(3 * frequencyData[index]))
-            else
-                vertex.set(vertex.x, -6 * (frequencyData[index] + 100))
-            index++
+            var bar = barGroup.children[i];
+            if (index >= 0 && index < 70)
+				bar.stroke = kickColor;
+
+            var vertex = bar.vertices[1];
+            vertex.set(vertex.x, -700 * (frequencyData[index]));
+
+            index++;
         }
-	}).play()
+        //*/
 
+        if (deltaFrameCount > 120)
+            deltaFrameCount = 0;
+        var dirChangeFactor = (deltaFrameCount / 800) + 0.005;
+        //console.log(dirChangeFactor);
 
-	
-	source.start(0)                             // play the source now
-												// note: on older systems, may have to use deprecated noteOn(time)
-}
+        var sign = Math.floor(frameCount / 300) % 2 == 0 ? 1 : -1;
+        velocityTheta = velocityTheta + (sign * dirChangeFactor);
+        targetBallVelocity.set(Math.cos(velocityTheta), Math.sin(velocityTheta));
+        //console.log(targetBallVelocity);
+        
+        var temp = 1;
+        if (timeDelta !== undefined)
+            temp = parseFloat(((timeDelta / 16.67).toFixed(2)));
+
+        targetBall.translation.set(targetBall.translation.x + targetBallVelocity.x * temp, targetBall.translation.y + targetBallVelocity.y * temp);
+        if (targetBall.translation.x < 50 || targetBall.translation.x > 1300) {
+            //targetBallVelocity.x = -targetBallVelocity.x;
+            velocityTheta += Math.PI;
+            targetBallVelocity.set(Math.cos(velocityTheta), Math.sin(velocityTheta));
+        }
+
+        if (targetBall.translation.y < 50 || targetBall.translation.y > 600) {
+            //targetBallVelocity.y = -targetBallVelocity.y;
+            velocityTheta += Math.PI;
+            targetBallVelocity.set(Math.cos(velocityTheta), Math.sin(velocityTheta));
+        }
+
+        targetBall.fill = kickColor;
+
+	}).play();
+};
 
 function init() {
 	try {
 		// fix up for prefixing
-		window.AudioContext = window.AudioContext || window.webkitAudioContext
-		context = new AudioContext()
-		loadSong('http://localhost:8000/getlucky')
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		context = new AudioContext();
+		loadSong('./songs/getlucky.mp3');
 	}
 	catch(e) {
-		alert('Web Audio API is not supported in this browser')
+		console.log(e.message);
+		console.log(e.stack);
+		//alert('Web Audio API is not supported in this browser');
 	}
 }
--- server.js
+++ server.js
@@ -1,39 +1,75 @@
-var http = require('http')
-var url = require('url')
-var fs = require('fs')
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer(function (request, response) {
     if (request.method == 'POST')
-        return console.error('reqest must be of type GET')
+        return console.error('reqest must be of type GET');
         
     
-    var query = url.parse(request.url, true).query
-    console.log(url.parse(request.url, true).pathname)
+    var query = url.parse(request.url, true).query;
+    console.log(url.parse(request.url, true).pathname);
     
     if (url.parse(request.url, true).pathname === '/') {
-        response.writeHead(200, { 'Content-Type': 'text/html' })
-        fs.createReadStream('./index.html').pipe(response)
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream('./index.html').pipe(response);
     }
     else if (url.parse(request.url, true).pathname === '/favicon.ico') {
-        response.writeHead(200, { 'Content-Type': 'image/x-icon' })
-        fs.createReadStream('./favicon.ico').pipe(response)
+        response.writeHead(200, { 'Content-Type': 'image/x-icon' });
+        fs.createReadStream('./favicon.ico').pipe(response);
     }
     else if (url.parse(request.url, true).pathname === '/two.js') {
-        response.writeHead(200, { 'Content-Type': 'application/javascript'})
-        fs.createReadStream('./two.js').pipe(response)
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./two.js').pipe(response);
+    }
+    else if (url.parse(request.url, true).pathname === '/src/dancer.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./src/dancer.js').pipe(response);
+    }
+    else if (url.parse(request.url, true).pathname === '/src/kick.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./src/kick.js').pipe(response);
+    }
+    else if (url.parse(request.url, true).pathname === '/src/adapterWebAudio.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./src/adapterWebAudio.js').pipe(response);
+    }
+    else if (url.parse(request.url, true).pathname === '/src/adapterMoz.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./src/adapterMoz.js').pipe(response);
+    }
+    else if (url.parse(request.url, true).pathname === '/src/adapterFlash.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./src/adapterFlash.js').pipe(response);
+    }
+    else if (url.parse(request.url, true).pathname === '/src/support.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./src/support.js').pipe(response);
+    }
+    else if (url.parse(request.url, true).pathname === '/lib/fft.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./lib/fft.js').pipe(response);
+    }
+    else if (url.parse(request.url, true).pathname === '/lib/flash_detect.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./lib/flash_detect.js').pipe(response);
+    }
+    else if (url.parse(request.url, true).pathname === '/plugins/dancer.fft.js') {
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./plugins/dancer.fft.js').pipe(response);
     }
     else if (url.parse(request.url, true).pathname === '/app.js') {
-        response.writeHead(200, { 'Content-Type': 'application/javascript'})
-        fs.createReadStream('./app.js').pipe(response)
+        response.writeHead(200, { 'Content-Type': 'application/javascript'});
+        fs.createReadStream('./app.js').pipe(response);
     }
-    else if (url.parse(request.url, true).pathname === '/getlucky') {
-        response.writeHead(200, { 'Content-Type': 'text/plain'})
-        fs.createReadStream('./songs/desire.mp3').pipe(response)
+    else if (url.parse(request.url, true).pathname === '/songs/getlucky.mp3') {
+        response.writeHead(200, { 'Content-Type': 'audio/mpeg'});
+        fs.createReadStream('./songs/happy.mp3').pipe(response);
     }
     else if (url.parse(request.url, true).pathname === '/undefined') {
-        response.writeHead(200, { 'Content-Type': 'text/plain'})
-        fs.createReadStream('./favicon.ico').pipe(response)
+        response.writeHead(200, { 'Content-Type': 'text/plain'});
+        fs.createReadStream('./favicon.ico').pipe(response);
     }
-})
+});
 
-server.listen(process.argv[2])
+server.listen(process.argv[2]);
